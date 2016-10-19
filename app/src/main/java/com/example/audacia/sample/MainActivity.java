84beefab76baf77f56179a5fcf18816fd8229a03
Jangho <johnjanghochoi@gmail.com>
2016-10-19 15:41:18 +0900

    Changed activity_my_road_map.xml

--- app/src/main/java/com/example/audacia/sample/MainActivity.java
+++ app/src/main/java/com/example/audacia/sample/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.audacia.sample;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 

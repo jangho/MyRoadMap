fa53a630ed7458b92dabc8057d32c89451390de7
Jangho <johnjanghochoi@gmail.com>
2016-10-19 14:16:56 +0900

    Add MapApiConst.java

---
+++ app/src/main/java/com/example/audacia/sample/MapApiConst.java
@@ -0,0 +1,10 @@
+package com.example.audacia.sample;
+
+/**
+ * Created by Jan on 2016. 10. 19..
+ */
+
+public class MapApiConst {
+    // http://developers.daum.net/console
+    public static final String DAUM_MAPS_ANDROID_APP_API_KEY = "847e6978474f073d697d46982b459a1f";
+}
